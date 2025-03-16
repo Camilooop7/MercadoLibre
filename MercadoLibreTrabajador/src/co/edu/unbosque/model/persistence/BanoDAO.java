daaf79582a44e48185865ee38d9324ffefcab737
BOSQUECILLO <sergiopastran16@gmail.com>
2025-03-16 17:38:08 -0500

    BANO DAO JAVADOC

--- MercadoLibreTrabajador/src/co/edu/unbosque/model/persistence/BanoDAO.java
+++ MercadoLibreTrabajador/src/co/edu/unbosque/model/persistence/BanoDAO.java
@@ -4,17 +4,39 @@ import java.util.ArrayList;
 
 import co.edu.unbosque.model.Bano;
 
+/*
+ * Clase en la cual se implementa la interfaz OperacionDAO
+ */
 public class BanoDAO implements OperacionDAO<Bano> {
+	/*
+	 * Se declara una variable constante definida por el final la cual 
+	 * no deja de ser modificada una vez inicializada y se le indica el 
+	 * tipo de archivo .dat
+	 * 
+	 */
 	private final String SERIAL_FILE_NAME = "bano.dat"; 
 	
-	private ArrayList<Bano> listaBanos = new ArrayList<>();
+	/*
+	 * Variable la cual contiene un arraylist y además se define el nombre.
+	 */
+	private ArrayList<Bano> listaBanos = new ArrayList<>(); // dos veces!!!!!!!!!!!
 	
+	
+	/*
+	 * Constructor en cual cual se llama al metodo de cargarSerializado y
+	 * se inicializa la Arraylist.
+	 */
 	public BanoDAO() {
 		// TODO Auto-generated constructor stub
 		listaBanos = new ArrayList<>();
 		cargarSerializado();
 	}
 	
+	
+	/*
+	 *Metodo crear el cual recibe como parametro nuevoDato y se recurre a la lista agregando el nuevoDato
+	 *además de eso se llama al metodo de escribirSerializado. 
+	 */
 	@Override
 	public void crear(Bano nuevoDato) {
 		// TODO Auto-generated method stub
@@ -22,6 +44,12 @@ public class BanoDAO implements OperacionDAO<Bano> {
         escribirSerializado();
 	}
 
+	/*
+	 *Metodo eliminar el cual recibe como parametro un dato tipo entero en el cual se incluye
+	 *un condicional tipo if, verifica y recorre los limites de las lista eliminando 
+	 *el objeto en la posicion indicada, si no devuelve el numero indicado si no se cumple el ciclo.
+	 *Se llama al metodo de escribirSerializado. 
+	 */
 	@Override
 	public int eliminar(int index) {
 		// TODO Auto-generated method stub
@@ -34,6 +62,13 @@ public class BanoDAO implements OperacionDAO<Bano> {
         }
 	}
 
+	/*
+	 *Metodo actualizar el cual recibe como parametro un dato tipo entero y nuevoDato en el cual se incluye
+	 *un condicional tipo if, verifica y recorre los limites de las lista actualizando 
+	 *el objeto en la posicion indicada con el .set recibiendo los parametros definidos, si no devuelve 
+	 *el numero indicado si no se cumple el ciclo.
+	 *Se llama al metodo de escribirSerializado. 
+	 */
 	@Override
 	public int actulizar(int index, Bano nuevoDato) {
 		// TODO Auto-generated method stub
@@ -46,6 +81,13 @@ public class BanoDAO implements OperacionDAO<Bano> {
         }
 	}
 
+	
+	/*
+	 *Metodo mosrtarTodo el cual no recibe parametros 
+	 *Se crea una variable tipo String y se recorre la lista con el uso del for 
+	 * y se realiza la concatenación de salida con el .toString, por ultimo se retorna la variable 
+	 * salida con su respectivo to string.
+	 */
 	@Override
 	public String mostrarTodo() {
 		// TODO Auto-generated method stub
@@ -56,11 +98,22 @@ public class BanoDAO implements OperacionDAO<Bano> {
         return salida.toString();
 	}
 	
+	
+	/*
+	 * Metodo el cual tiene como objetivo guardar la lista definida dentro del archivo como .dat
+	 */
 	@Override
 	public void escribirSerializado() {
 		FileManager.escribirArchivoSerializado(SERIAL_FILE_NAME,listaBanos);
 	}
 	
+	
+	/*
+	 * Este metodo carga el archivo la lista en el archivoSerializado
+	 * donde se lee la lista con el leerArchivoSerializado en este caso SERIAL_FILE_NAME.
+	 * Se utiliza un condiconal tipo if en donde si la lista se encuentra vacia se inicializa 
+	 * un nuevo array para la lista.
+	 */
 	@Override
 	public void cargarSerializado() {
 		listaBanos = (ArrayList<Bano>) FileManager.leerArchivoSerialziado(SERIAL_FILE_NAME);
