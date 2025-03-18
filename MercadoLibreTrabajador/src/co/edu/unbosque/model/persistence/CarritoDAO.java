3267775f50bdf67b2fc5665b5bbc44c31c45b58e
Sealpaca-16 <sergiopastran16@gmail.com>
2025-03-17 21:05:42 -0500

    COMENTARIO JAVADOC EN DAO HASTA  ELECTRODOMESTICO

--- MercadoLibreTrabajador/src/co/edu/unbosque/model/persistence/CarritoDAO.java
+++ MercadoLibreTrabajador/src/co/edu/unbosque/model/persistence/CarritoDAO.java
@@ -3,69 +3,123 @@ package co.edu.unbosque.model.persistence;
 import java.util.ArrayList;
 import co.edu.unbosque.model.Carrito;
 
+/*
+ * Clase en la cual se implementa la interfaz OperacionDAO
+ */
 public class CarritoDAO implements OperacionDAO<Carrito> {
-    private final String SERIAL_FILE_NAME = "carrito.dat";
-    private ArrayList<Carrito> listaCarritos;
-    
+
+	/*
+	 * Se declara una variable constante definida por el final la cual no deja de
+	 * ser modificada una vez inicializada y se le indica el tipo de archivo .dat
+	 * 
+	 */
+	private final String SERIAL_FILE_NAME = "carrito.dat";
+
+	/*
+	 * Variable la cual contiene un arraylist y además se define el nombre.
+	 */
+	private ArrayList<Carrito> listaCarritos;
+
+	/*
+	 * Constructor en cual cual se llama al metodo de cargarSerializado y se
+	 * inicializa la Arraylist.
+	 */
 	public CarritoDAO() {
 		// TODO Auto-generated constructor stub
 		listaCarritos = new ArrayList<>();
 		cargarSerializado();
 	}
-    @Override
-    public void crear(Carrito nuevoDato) {
-        listaCarritos.add(nuevoDato);
-        escribirSerializado();
-    }
 
-    @Override
-    public int eliminar(int index) {
-        if (index >= 0 && index < listaCarritos.size()) {
-            listaCarritos.remove(index);
-            escribirSerializado();
-            return 0;
-        } else {
-            return 1;
-        }
-    }
+	/*
+	 * Metodo crear el cual recibe como parametro nuevoDato y se recurre a la lista
+	 * agregando el nuevoDato además de eso se llama al metodo de
+	 * escribirSerializado.
+	 */
+	@Override
+	public void crear(Carrito nuevoDato) {
+		listaCarritos.add(nuevoDato);
+		escribirSerializado();
+	}
 
-    @Override
-    public int actulizar(int index, Carrito nuevoDato) {
-        if (index >= 0 && index < listaCarritos.size()) {
-            listaCarritos.set(index, nuevoDato);
-            escribirSerializado();
-            return 0;
-        } else {
-            return 1;
-        }
-    }
+	/*
+	 * Metodo eliminar el cual recibe como parametro un dato tipo entero en el cual
+	 * se incluye un condicional tipo if, verifica y recorre los limites de las
+	 * lista eliminando el objeto en la posicion indicada, si no devuelve el numero
+	 * indicado si no se cumple el ciclo. Se llama al metodo de escribirSerializado.
+	 */
+	@Override
+	public int eliminar(int index) {
+		if (index >= 0 && index < listaCarritos.size()) {
+			listaCarritos.remove(index);
+			escribirSerializado();
+			return 0;
+		} else {
+			return 1;
+		}
+	}
 
-    @Override
-    public String mostrarTodo() {
-        String salida = "";
-        for (Carrito carrito : listaCarritos) {
-            salida += carrito.toString();
-        }
-        return salida;
-    }
+	/*
+	 * Metodo actualizar el cual recibe como parametro un dato tipo entero y
+	 * nuevoDato en el cual se incluye un condicional tipo if, verifica y recorre
+	 * los limites de las lista actualizando el objeto en la posicion indicada con
+	 * el .set recibiendo los parametros definidos, si no devuelve el numero
+	 * indicado si no se cumple el ciclo. Se llama al metodo de escribirSerializado.
+	 */
+	@Override
+	public int actulizar(int index, Carrito nuevoDato) {
+		if (index >= 0 && index < listaCarritos.size()) {
+			listaCarritos.set(index, nuevoDato);
+			escribirSerializado();
+			return 0;
+		} else {
+			return 1;
+		}
+	}
 
-    @Override
-    public void escribirSerializado() {
-        FileManager.escribirArchivoSerializado(SERIAL_FILE_NAME, listaCarritos);
-    }
+	/*
+	 * Metodo mosrtarTodo el cual no recibe parametros Se crea una variable tipo
+	 * String y se recorre la lista con el uso del for y se realiza la concatenación
+	 * de salida con el .toString, por ultimo se retorna la variable salida con su
+	 * respectivo to string.
+	 */
+	@Override
+	public String mostrarTodo() {
+		String salida = "";
+		for (Carrito carrito : listaCarritos) {
+			salida += carrito.toString();
+		}
+		return salida;
+	}
 
-    @Override
-    public void cargarSerializado() {
-        listaCarritos = (ArrayList<Carrito>) FileManager.leerArchivoSerialziado(SERIAL_FILE_NAME);
-        if (listaCarritos == null) {
-            listaCarritos = new ArrayList<>();
-        }
-    }
+	/*
+	 * Metodo el cual tiene como objetivo guardar la lista definida dentro del
+	 * archivo como .dat
+	 */
+	@Override
+	public void escribirSerializado() {
+		FileManager.escribirArchivoSerializado(SERIAL_FILE_NAME, listaCarritos);
+	}
+
+	/*
+	 * Este metodo carga el archivo la lista en el archivoSerializado donde se lee
+	 * la lista con el leerArchivoSerializado en este caso SERIAL_FILE_NAME. Se
+	 * utiliza un condiconal tipo if en donde si la lista se encuentra vacia se
+	 * inicializa un nuevo array para la lista.
+	 */
+	@Override
+	public void cargarSerializado() {
+		listaCarritos = (ArrayList<Carrito>) FileManager.leerArchivoSerialziado(SERIAL_FILE_NAME);
+		if (listaCarritos == null) {
+			listaCarritos = new ArrayList<>();
+		}
+	}
+
 	public ArrayList<Carrito> getListaCarritos() {
 		return listaCarritos;
 	}
+
 	public void setListaCarritos(ArrayList<Carrito> listaCarritos) {
 		this.listaCarritos = listaCarritos;
 	}
-    
+
 }
--- MercadoLibreTrabajador/src/co/edu/unbosque/model/persistence/ClienteDAO.java
+++ MercadoLibreTrabajador/src/co/edu/unbosque/model/persistence/ClienteDAO.java
@@ -3,22 +3,50 @@ package co.edu.unbosque.model.persistence;
 import java.util.ArrayList;
 import co.edu.unbosque.model.Cliente;
 
+/*
+ * Clase en la cual se implementa la interfaz OperacionDAO
+ */
 public class ClienteDAO implements OperacionDAO<Cliente> {
+	
+	/*
+	 * Se declara una variable constante definida por el final la cual no deja de
+	 * ser modificada una vez inicializada y se le indica el tipo de archivo .dat
+	 * 
+	 */
     private final String SERIAL_FILE_NAME = "cliente.dat";
+    
+    /*
+	 * Variable la cual contiene un arraylist y además se define el nombre.
+	 */
     private ArrayList<Cliente> listaClientes;
 
+    /*
+	 * Constructor en cual cual se llama al metodo de cargarSerializado y se
+	 * inicializa la Arraylist.
+	 */
     public ClienteDAO() {
 		// TODO Auto-generated constructor stub
 		listaClientes = new ArrayList<>();
 		cargarSerializado();
 	}
     
+    /*
+	 * Metodo crear el cual recibe como parametro nuevoDato y se recurre a la lista
+	 * agregando el nuevoDato además de eso se llama al metodo de
+	 * escribirSerializado.
+	 */
     @Override
     public void crear(Cliente nuevoDato) {
         listaClientes.add(nuevoDato);
         escribirSerializado();
     }
 
+    /*
+	 * Metodo eliminar el cual recibe como parametro un dato tipo entero en el cual
+	 * se incluye un condicional tipo if, verifica y recorre los limites de las
+	 * lista eliminando el objeto en la posicion indicada, si no devuelve el numero
+	 * indicado si no se cumple el ciclo. Se llama al metodo de escribirSerializado.
+	 */
     @Override
     public int eliminar(int index) {
         if (index >= 0 && index < listaClientes.size()) {
@@ -30,6 +58,13 @@ public class ClienteDAO implements OperacionDAO<Cliente> {
         }
     }
 
+    /*
+	 * Metodo actualizar el cual recibe como parametro un dato tipo entero y
+	 * nuevoDato en el cual se incluye un condicional tipo if, verifica y recorre
+	 * los limites de las lista actualizando el objeto en la posicion indicada con
+	 * el .set recibiendo los parametros definidos, si no devuelve el numero
+	 * indicado si no se cumple el ciclo. Se llama al metodo de escribirSerializado.
+	 */
     @Override
     public int actulizar(int index, Cliente nuevoDato) {
         if (index >= 0 && index < listaClientes.size()) {
@@ -41,6 +76,12 @@ public class ClienteDAO implements OperacionDAO<Cliente> {
         }
     }
 
+    /*
+	 * Metodo mosrtarTodo el cual no recibe parametros Se crea una variable tipo
+	 * String y se recorre la lista con el uso del for y se realiza la concatenación
+	 * de salida con el .toString, por ultimo se retorna la variable salida con su
+	 * respectivo to string.
+	 */
     @Override
     public String mostrarTodo() {
         String salida = "";
@@ -50,11 +91,21 @@ public class ClienteDAO implements OperacionDAO<Cliente> {
         return salida;
     }
 
+    /*
+	 * Metodo el cual tiene como objetivo guardar la lista definida dentro del
+	 * archivo como .dat
+	 */
     @Override
     public void escribirSerializado() {
         FileManager.escribirArchivoSerializado(SERIAL_FILE_NAME, listaClientes);
     }
 
+    /*
+	 * Este metodo carga el archivo la lista en el archivoSerializado donde se lee
+	 * la lista con el leerArchivoSerializado en este caso SERIAL_FILE_NAME. Se
+	 * utiliza un condiconal tipo if en donde si la lista se encuentra vacia se
+	 * inicializa un nuevo array para la lista.
+	 */
     @Override
     public void cargarSerializado() {
         listaClientes = (ArrayList<Cliente>) FileManager.leerArchivoSerialziado(SERIAL_FILE_NAME);
