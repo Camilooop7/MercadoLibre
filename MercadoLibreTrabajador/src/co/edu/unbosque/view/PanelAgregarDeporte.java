f5f24457d6f32191faaa618aebf0439b39377d57
HatosJeisson <jeissoneliasherresil@gmail.com>
2025-03-23 22:27:15 -0500

    paneles Deporte y videojuego

--- MercadoLibreTrabajador/src/co/edu/unbosque/view/PanelAgregarDeporte.java
+++ MercadoLibreTrabajador/src/co/edu/unbosque/view/PanelAgregarDeporte.java
@@ -1,21 +1,35 @@
 package co.edu.unbosque.view;
 
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
+import javax.swing.ButtonGroup;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.JSpinner;
+import javax.swing.JTextField;
 
 public class PanelAgregarDeporte extends JPanel {
 
 	private JLabel fondo;
 	private JButton btnAgregar;
+	private JLabel textNombre;
+	private JTextField nombre;
+	private JLabel textPrecio;
+	private JSpinner precio;
+	private JLabel textAccesorio;
+	private JRadioButton siA;
+	private JRadioButton noA;
+	private JLabel textDeporte;
+	private JTextField deporte;
 	
 	public PanelAgregarDeporte() throws IOException {
 
@@ -38,6 +52,91 @@ public class PanelAgregarDeporte extends JPanel {
 		btnAgregar.setBorderPainted(false);
 		btnAgregar.setVisible(true);
 		add(btnAgregar);
+		
+		textNombre = new JLabel();
+		textNombre.setBounds(44, 40, 150, 60);
+		textNombre.setText("Nombre: ");
+		textNombre.setFont(new Font("Baloo", Font.BOLD, 24));
+		add(textNombre);
+
+		nombre = new JTextField();
+		nombre.setBounds(140, 40, 490, 50);
+		nombre.setFont(new Font("Baloo", Font.BOLD, 24));
+		add(nombre);
+
+		textPrecio = new JLabel();
+		textPrecio.setBounds(50, 130, 150, 60);
+		textPrecio.setText("Precio: ");
+		textPrecio.setFont(new Font("Baloo", Font.BOLD, 24));
+		add(textPrecio);
+
+		precio = new JSpinner();
+		precio.setBounds(140, 130, 490, 50);
+		precio.setFont(new Font("Baloo", Font.BOLD, 24));
+		add(precio);
+
+		textNombre = new JLabel();
+		textNombre.setBounds(44, 40, 150, 60);
+		textNombre.setText("Nombre: ");
+		textNombre.setFont(new Font("Baloo", Font.BOLD, 24));
+		add(textNombre);
+
+		nombre = new JTextField();
+		nombre.setBounds(140, 40, 490, 50);
+		nombre.setFont(new Font("Baloo", Font.BOLD, 24));
+		add(nombre);
+
+		textPrecio = new JLabel();
+		textPrecio.setBounds(50, 130, 150, 60);
+		textPrecio.setText("Precio: ");
+		textPrecio.setFont(new Font("Baloo", Font.BOLD, 24));
+		add(textPrecio);
+
+		precio = new JSpinner();
+		precio.setBounds(140, 130, 490, 50);
+		precio.setFont(new Font("Baloo", Font.BOLD, 24));
+		add(precio);
+
+		textAccesorio = new JLabel();
+		textAccesorio.setBounds(50, 190, 400, 60);
+		textAccesorio.setText("¿Es accesorio? ");
+		textAccesorio.setFont(new Font("Baloo", Font.BOLD, 26));
+		add(textAccesorio);
+
+		siA = new JRadioButton("SI");
+		siA.setBounds(257, 210, 80, 30); // Ajustar el tamaño para que el texto sea visible
+		siA.setFont(new Font("Baloo", Font.BOLD, 15));
+		siA.setOpaque(false);
+		siA.setContentAreaFilled(false);
+		siA.setBorderPainted(false);
+		siA.setFocusPainted(false);
+		add(siA);
+
+		noA = new JRadioButton("NO");
+		noA.setBounds(340, 210, 80, 30); // Ajustar el tamaño para que el texto sea visible
+		noA.setFont(new Font("Baloo", Font.BOLD, 15));
+		noA.setOpaque(false);
+		noA.setContentAreaFilled(false);
+		noA.setBorderPainted(false);
+		noA.setFocusPainted(false);
+		add(noA);
+		// Agrupar los JRadioButton
+		ButtonGroup grupo = new ButtonGroup();
+		grupo.add(siA);
+		grupo.add(noA);
+		
+		textDeporte = new JLabel();
+		textDeporte.setBounds(40, 270, 150, 60);
+		textDeporte.setText("Deporte:");
+		textDeporte.setFont(new Font("Baloo", Font.BOLD, 24));
+		add(textDeporte);
+
+		deporte = new JTextField();
+		deporte.setBounds(140, 270, 470, 50);
+		deporte.setFont(new Font("Baloo", Font.BOLD, 24));
+		add(deporte);
+
+		
 		
 		add(fondo);
 	}
--- MercadoLibreTrabajador/src/co/edu/unbosque/view/PanelAgregarVideoJuego.java
+++ MercadoLibreTrabajador/src/co/edu/unbosque/view/PanelAgregarVideoJuego.java
@@ -1,21 +1,35 @@
 package co.edu.unbosque.view;
 
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
+import javax.swing.ButtonGroup;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.JSpinner;
+import javax.swing.JTextField;
 
 public class PanelAgregarVideoJuego extends JPanel {
 
 	private JLabel fondo;
 	private JButton btnAgregar;
+	private JLabel textNombre;
+	private JTextField nombre;
+	private JLabel textPrecio;
+	private JSpinner precio;
+	private JLabel textAccesorio;
+	private JRadioButton siA;
+	private JRadioButton noA;
+	private JLabel textRefeciaConsola;
+	private JTextField referenciaConsola;
 
 	public PanelAgregarVideoJuego() throws IOException {
 
@@ -38,8 +52,161 @@ public class PanelAgregarVideoJuego extends JPanel {
 		btnAgregar.setBorderPainted(false);
 		btnAgregar.setVisible(true);
 		add(btnAgregar);
+		
+		textNombre = new JLabel();
+		textNombre.setBounds(44, 40, 150, 60);
+		textNombre.setText("Nombre: ");
+		textNombre.setFont(new Font("Baloo", Font.BOLD, 24));
+		add(textNombre);
+
+		nombre = new JTextField();
+		nombre.setBounds(140, 40, 490, 50);
+		nombre.setFont(new Font("Baloo", Font.BOLD, 24));
+		add(nombre);
+
+		textPrecio = new JLabel();
+		textPrecio.setBounds(50, 130, 150, 60);
+		textPrecio.setText("Precio: ");
+		textPrecio.setFont(new Font("Baloo", Font.BOLD, 24));
+		add(textPrecio);
+
+		precio = new JSpinner();
+		precio.setBounds(140, 130, 490, 50);
+		precio.setFont(new Font("Baloo", Font.BOLD, 24));
+		add(precio);
+
+		textAccesorio = new JLabel();
+		textAccesorio.setBounds(50, 190, 400, 60);
+		textAccesorio.setText("¿Es accesorio? ");
+		textAccesorio.setFont(new Font("Baloo", Font.BOLD, 26));
+		add(textAccesorio);
+
+		siA = new JRadioButton("SI");
+		siA.setBounds(257, 210, 80, 30); // Ajustar el tamaño para que el texto sea visible
+		siA.setFont(new Font("Baloo", Font.BOLD, 15));
+		siA.setOpaque(false);
+		siA.setContentAreaFilled(false);
+		siA.setBorderPainted(false);
+		siA.setFocusPainted(false);
+		add(siA);
+
+		noA = new JRadioButton("NO");
+		noA.setBounds(340, 210, 80, 30); // Ajustar el tamaño para que el texto sea visible
+		noA.setFont(new Font("Baloo", Font.BOLD, 15));
+		noA.setOpaque(false);
+		noA.setContentAreaFilled(false);
+		noA.setBorderPainted(false);
+		noA.setFocusPainted(false);
+		add(noA);
+		// Agrupar los JRadioButton
+		ButtonGroup grupo = new ButtonGroup();
+		grupo.add(siA);
+		grupo.add(noA);
+		
+		textRefeciaConsola = new JLabel();
+		textRefeciaConsola.setBounds(30, 270, 220, 60);
+		textRefeciaConsola.setText("Referencia consola:");
+		textRefeciaConsola.setFont(new Font("Baloo", Font.BOLD, 22));
+		add(textRefeciaConsola);
+
+		referenciaConsola = new JTextField();
+		referenciaConsola.setBounds(270, 270, 340, 50);
+		referenciaConsola.setFont(new Font("Baloo", Font.BOLD, 24));
+		add(referenciaConsola);
+
+		
 
 		add(fondo);
 	}
+
+	public JLabel getFondo() {
+		return fondo;
+	}
+
+	public void setFondo(JLabel fondo) {
+		this.fondo = fondo;
+	}
+
+	public JButton getBtnAgregar() {
+		return btnAgregar;
+	}
+
+	public void setBtnAgregar(JButton btnAgregar) {
+		this.btnAgregar = btnAgregar;
+	}
+
+	public JLabel getTextNombre() {
+		return textNombre;
+	}
+
+	public void setTextNombre(JLabel textNombre) {
+		this.textNombre = textNombre;
+	}
+
+	public JTextField getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(JTextField nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getTextPrecio() {
+		return (String) nombre.getText();
+	}
+
+	public void setTextPrecio(JLabel textPrecio) {
+		this.textPrecio = textPrecio;
+	}
+
+	public int getPrecio() {
+		return (int) precio.getValue();
+	}
+
+	public void setPrecio(JSpinner precio) {
+		this.precio = precio;
+	}
+
+	public JLabel getTextAccesorio() {
+		return textAccesorio;
+	}
+
+	public void setTextAccesorio(JLabel textAccesorio) {
+		this.textAccesorio = textAccesorio;
+	}
+
+	public JRadioButton getSiA() {
+		return siA;
+	}
+
+	public void setSiA(JRadioButton siA) {
+		this.siA = siA;
+	}
+
+	public JRadioButton getNoA() {
+		return noA;
+	}
+
+	public void setNoA(JRadioButton noA) {
+		this.noA = noA;
+	}
+
+	public JLabel getTextRefeciaConsola() {
+		return textRefeciaConsola;
+	}
+
+	public void setTextRefeciaConsola(JLabel textRefeciaConsola) {
+		this.textRefeciaConsola = textRefeciaConsola;
+	}
+
+	public String getReferenciaConsola() {
+		return (String) nombre.getText();
+	}
+
+	public void setReferenciaConsola(JTextField referenciaConsola) {
+		this.referenciaConsola = referenciaConsola;
+	}
+	
+	
 
 }
