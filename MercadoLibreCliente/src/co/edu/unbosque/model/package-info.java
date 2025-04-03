f3017198cc102c39f457c2b26cb5bbdce2305494
LuisVivasC <lc.vivascruz@gmail.com>
2025-04-02 19:29:20 -0500

    MercadoLibreClienteAcabado

---
+++ MercadoLibreCliente/src/co/edu/unbosque/model/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Este paquete contiene las clases relacionadas co todos los objetos y los datos de la aplicación.
+ * Incluye las entidades principales y sus operaciones.
+ */
+package co.edu.unbosque.model;
---
+++ MercadoLibreCliente/src/co/edu/unbosque/util/exception/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Este paquete contiene las clases relacionadas con el manejo de excepciones
+ * personalizadas para la aplicación MercadoLibreCliente.
+ */
+package co.edu.unbosque.util.exception;
